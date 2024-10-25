8bd17355f9b35ed2a207914340ab070cc0667ba6
valentina annecchini <valentinannecchini@gmail.com>
2024-10-25 16:45:46 -0300

    fix vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -6,5 +6,9 @@ module.exports = defineConfig({
     allowedHosts: 'all', // Permite todos los hosts
     host: '0.0.0.0', // Escucha en todas las interfaces
     port: process.env.PORT || 8080, // Asegúrate de usar el puerto correcto
+    https: true, // Habilita HTTPS
+    client: {
+      webSocketURL: 'wss://0.0.0.0:8080/ws', // Cambia el URL de WebSocket a WSS
+    },
   },
 });
