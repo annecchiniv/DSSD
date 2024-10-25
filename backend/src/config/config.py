f3e52dcadb1ed3b3dc5a2475d44f2d6d75623263
valentina annecchini <valentinannecchini@gmail.com>
2024-10-25 17:37:35 -0300

    cambios

--- backend/src/config/config.py
+++ backend/src/config/config.py
@@ -1,16 +1,19 @@
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 load_dotenv()
 
 class Config:
-    USER = os.getenv('MYSQLUSER')  # Asegúrate de que este nombre coincida
-    PASSWORD = os.getenv('MYSQLPASSWORD')  # Lo mismo aquí
-    HOST = os.getenv('MYSQLHOST')  # Asegúrate de que esto esté configurado correctamente
-    DB_NAME = os.getenv('MYSQL_DATABASE')  # Cambia esto si es necesario
-    
-    # Define la cadena de conexión usando las variables de Railway
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{os.getenv("MYSQLPORT")}/{DB_NAME}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Si Railway proporciona `DATABASE_URL`, úsalo; de lo contrario, usa las variables individuales.
+    if os.getenv("DATABASE_URL"):
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("mysql://", "mysql+pymysql://")
+    else:
+        USER = os.getenv('MYSQLUSER')
+        PASSWORD = os.getenv('MYSQLPASSWORD')
+        HOST = os.getenv('MYSQLHOST')
+        DB_NAME = os.getenv('MYSQL_DATABASE')
+        MYSQLPORT = os.getenv("MYSQLPORT")
+        SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{MYSQLPORT}/{DB_NAME}'
 
-    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
