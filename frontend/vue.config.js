f8837a9606f866155fa6b76ae5c6787c33b9ca49
valentina annecchini <valentinannecchini@gmail.com>
2024-10-25 16:43:08 -0300

    cambiando vue.config.js por allowedHosts

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-})
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  devServer: {
+    allowedHosts: 'all', // Permite todos los hosts
+    host: '0.0.0.0', // Escucha en todas las interfaces
+    port: process.env.PORT || 8080, // Asegúrate de usar el puerto correcto
+  },
+});
