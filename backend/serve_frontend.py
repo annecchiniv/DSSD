09551ddfd2926f5d82daf21c8e42ed870bb1d3af
valentina <valentinannecchini@gmail.com>
2024-10-25 04:05:06 -0300

    cambios en init.py

--- backend/serve_frontend.py
+++ backend/serve_frontend.py
@@ -1,6 +1,8 @@
 from flask import send_from_directory
-from app import app 
+import os
 
-@app.route('/')
-def serve_frontend():
-    return send_from_directory('../frontend/dist', 'index.html')
+def serve_frontend(app):
+    @app.route('/')
+    def serve_index():
+        dist_path = os.path.join(app.root_path, '../frontend/dist')
+        return send_from_directory('../frontend/dist', 'index.html')
